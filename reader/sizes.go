cfe1cdc91754e0575afde1a58f6c1b3d7c73a060
Joe Scanlon <joseph.scanlon@oracle.com>
2023-05-08 08:16:45 -0400

    clear

--- reader/sizes.go
+++ reader/sizes.go
@@ -2,20 +2,23 @@ package reader
 
 import (
 	"context"
+	"fmt"
 	"myworkspace/core"
 	"strconv"
 
 	"log"
 
 	"github.com/oracle/oci-go-sdk/v65/common"
+	"github.com/oracle/oci-go-sdk/v65/example/helpers"
 	"github.com/oracle/oci-go-sdk/v65/objectstorage"
 )
 
 func GetSizes(connobj core.ConnectionObj) {
 
 	getObjectCount(connobj.NameSpace, connobj.Config.Source.Bucketname, connobj.SourceClient)
-
+	GetReplicationPolicy(connobj.NameSpace, connobj.Config.Source.Bucketname, connobj.SourceClient)
 	getObjectCount(connobj.NameSpace, connobj.Config.Target.Bucketname, connobj.TargetClient)
+	GetReplicationPolicy(connobj.NameSpace, connobj.Config.Target.Bucketname, connobj.TargetClient)
 
 }
 
@@ -43,4 +46,22 @@ func getObjectCount(namespace, bucketName string, objectStorageClient objectstor
 
 	log.Printf("bucket %v in region %v has approximately %s objects\n", bucketName, objectStorageClient.Endpoint(), strconv.FormatInt(int64(*objectCount), 10))
 
+}
+
+// GetReplicationPolicy retrieves the replication policy for a bucket
+func GetReplicationPolicy(namespace string, bucketName string, client objectstorage.ObjectStorageClient) {
+
+	// Create a request and dependent object(s).
+
+	req := objectstorage.ListReplicationPoliciesRequest{
+		BucketName:    &bucketName,
+		NamespaceName: &namespace,
+	}
+
+	// Send the request using the service client
+	resp, err := client.ListReplicationPolicies(context.Background(), req)
+	helpers.FatalIfError(err)
+
+	// Retrieve value from the response.
+	fmt.Println(resp)
 }
